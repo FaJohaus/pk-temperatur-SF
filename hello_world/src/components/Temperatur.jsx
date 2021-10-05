67b9d43a76f2439b2842d920925a4804a6196380
Fabian5150 <fabian.strauch.04@web.de>
2021-10-05 12:41:12 +0200

    added semantic ui elements to temp calc

--- hello_world/src/components/Temperatur.jsx
+++ hello_world/src/components/Temperatur.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 function Temperatur() {
     const [eingabe, setEingabe] = useState("");
     const [ergebnis, setErgebnis] = useState("");
-    const [bgCol, setBgColor] = useState("");
+    const [Color, setColor] = useState("");
     const [bild, setImg] = useState("");
 
     const handleChange = (event) => {
@@ -13,27 +13,48 @@ function Temperatur() {
     const handleClick = () => {
         setErgebnis(eingabe * 9/5 + 32);
         if(eingabe < 0){
-            setBgColor("blue");
+            setColor("blue");
             setImg("https://img.welt.de/img/kaufberatung/mobile207548979/9361354597-ci16x9-w1200/FROZEN-II-aka-FROZEN-2-poster-in-Spanish.jpg");
         } else if(eingabe <= 20){
-            setBgColor("cyan");
+            setColor("cyan");
             setImg("https://i.ytimg.com/vi/6Xyfju8H7Ys/maxresdefault.jpg");
         } else if(eingabe <= 28){
-            setBgColor("green");
+            setColor("green");
             setImg("https://www.dailydot.com/wp-content/uploads/364/04/c816f6bf7ea8b526652487e3479943cc.jpg");
         } else{
-            setBgColor("red");
+            setColor("red");
             setImg("https://everythingisviral.com/wp-content/uploads/2021/05/this-is-fine-meme-1024x722.jpg");
         }
     }
 
     return (<>
-        <input value={eingabe} onChange={handleChange}/>
+        <div class="ui placeholder segment">
+        <div class="ui two column very relaxed stackable grid">
+            <div class="column">
+            <div class="ui form">
+                <div class="field">
+                <div class="tempr">
+            <div class="ui input">
+                <input type="text" value={eingabe} onChange={handleChange}/>
+            </div>
 
-        <button onClick={handleClick}>Temperatur in Fahrenheit</button>
-    
-        <div style={{backgroundColor: bgCol}}>{ergebnis}</div> 
-        <img src={bild} width="300px"/>
+            <button class="ui primary basic button" onClick={handleClick}>Temperatur in Fahrenheit</button>
+                </div>
+                </div>
+            </div>
+            </div>
+            <div class="middle aligned column">
+            <div class="tempr">
+                <div style={{color: Color}}>{ergebnis}</div> 
+                <img class="ui medium circular image" src={bild} src={bild}img/>
+            </div>
+            </div>
+        </div>
+        <div class="ui vertical divider">
+
+        </div>
+        </div>
+        
     </>)
 }
 
